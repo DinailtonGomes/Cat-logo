258573878672768e956a39cb4b0bb7201368edba
Dinailton88 <dinailton88@hotmail.com>
2022-05-10 22:09:58 -0300

    correcao_BUg

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,21 @@
-import  Sequelize  from 'sequelize';
-import dotenv from 'dotenv'
+
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
 dotenv.config()
 
-export const connection =  new Sequelize(
-    process.env.DB_URL          
+export const connection = new Sequelize(
 
+    'postgres://discografia_user:HnY9MMA9ukJJMKLxtxJxM6GjFowDwKLx@dpg-c9t2s4j19n07ml7tlqbg-a.oregon-postgres.render.com/discografia', {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
 )
-
 
 
  
